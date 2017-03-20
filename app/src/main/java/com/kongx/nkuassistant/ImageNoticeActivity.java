7889517d9ba846759ba57940ceb36cbc118bfa13
kongxiao0532 <kongxiao0532@163.com>
2017-03-20 20:44:56 +0800

    add image notice fragment activity

---
+++ app/src/main/java/com/kongx/nkuassistant/ImageNoticeActivity.java
@@ -0,0 +1,28 @@
+package com.kongx.nkuassistant;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ImageNoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_notice);
+
+    }
+}
